5a0d6d4680d9fdbab7fb485d09156969b7a7de95
marshall <mhamad.marshall@gmail.com>
2022-10-20 10:02:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here
+main.remove()
+const newHeader = document.createElement("H1");
+const id = document.createElement("victory")
+newHeader.id = "victory"
+let text ="mhamad is the champion";
+newHeader.innerHTML = text
